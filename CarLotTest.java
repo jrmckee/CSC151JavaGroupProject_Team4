56353bfd6b4047981741d0c62f37822497e86444
jrmckee <106885317+jrmckee@users.noreply.github.com>
2022-07-08 11:47:27 -0400

    Update CarLotTest.java
    added some pseudocode to help with writing the actual code

--- CarLotTest.java
+++ CarLotTest.java
@@ -3,5 +3,17 @@ public class CarLotTest {
   public static void main(String[] args{
   
     CarLot newLot = new CarLot();
+    
+    // print all the cars in the lot in a tabular form
+    
+    // sell two cars 
+
+    // get the one with the highest MPG and display car to the user
+
+    // get the car with the highest mileage and display it to user
+
+    // get the average MPG of all cars in the lot
+
+    // get the profit of the cars that have been sold and display to user
   }
 }
